0c8025bfbf4c2183e6815a2cddcac39661cc513a
baptiste vigneron <vigneron.baptiste@live.fr>
2022-05-09 15:44:32 +0200

    create card plant and event on click

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,46 +1,34 @@
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/Shoppinglist.css'
-import { plantList } from "../datas/plantList";
 
-function ShoppingList(){
-  const categories = plantList.reduce((acc, plant) =>
-    acc.includes(plant.category) ? acc : acc.concat(plant.category), []
-  )
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-  const soldes = plantList.map((plant) =>
-    plant.isSpecialOffer ? plant.name : null
-    )
-
-  return (
-    <div>
-      <h2>Category</h2>
-      <ul>
-        {categories.map((category) => (
-          <li key={category}><a href='#'>{category}</a></li>
-        ))}
-      </ul>
-      <h2>Name</h2>
-      <ul>
-        {plantList.map((plant) => (
-          <li key= {plant.id}>
-            <a href=''>{plant.name}
-                        {plant.isBestSale && <span>🔥</span>}
-                        {plant.isSpecialOffer && <span>🤑</span>}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <h2>Soldes</h2>
-            {soldes.map((solde) =>(
-              <li key={solde}>
-                <a href='#'><div class="lmj-sales">{solde}</div></a>
-              </li>
-            ))}
-    </div>
-
-  )
-
-};
-
-
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
+				))}
+			</ul>
+		</div>
+	)
+}
 
 export default ShoppingList
--- src/datas/plantList.js
+++ src/datas/plantList.js
@@ -1,74 +1,95 @@
+import monstera from '../assets/monstera.jpg'
+import ficus from '../assets/ficus.jpg'
+import pothos from '../assets/pothos.jpg'
+import yuka from '../assets/yuka.jpg'
+import olivier from '../assets/olivier.jpg'
+import geranium from "../assets/geranium.jpg"
+import basilique from '../assets/basilique.jpg'
+import aloe from '../assets/aloe.jpg'
+import succulente from '../assets/succulente.jpg'
+import avocat from '../assets/avocat.jpeg'
+
 export const plantList = [
   {
-    name: 'monstera',
-    category: 'classique',
-    id: '1',
-    isBestSale: true,
-    isSpecialOffer: true
-  },
-
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		isBestSale: true,
+		light: 2,
+		water: 3,
+		cover: monstera
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: ficus
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		light: 1,
+		water: 2,
+		cover: pothos
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+		light: 3,
+		water: 1,
+		cover: yuka
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		light: 3,
+		water: 1,
+		cover: olivier
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: geranium
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		isBestSale: true,
+		light: 2,
+		water: 3,
+		cover: basilique
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: aloe
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: succulente
+	},
   {
-    name: 'ficus lyrata',
-    category: 'classique',
-    id: '2',
-    isBestSale: false,
-    isSpecialOffer: false
-  },
-
-  {
-    name: 'pothos argenté',
-    category: 'classique',
-    id: '3',
-    isBestSale: false,
-    isSpecialOffer: true
-  },
-
-  {
-    name: 'yucca',
-    category: 'classique',
-    id: '4',
-    isBestSale: true,
-    isSpecialOffer: false
-  },
-
-  {
-    name: 'olivier',
+    name: 'avocat',
     category: 'extérieur',
-    id: '5',
-    isBestSale: true,
-    isSpecialOffer: true
-  },
-
-  {
-    name: 'géranium',
-    category: 'extérieur',
-    id: '6',
-    isBestSale: true,
-    isSpecialOffer: false
-
-  },
-
-  {
-    name: 'basilique',
-    category: 'extérieur',
-    id: '7',
-    isBestSale: false,
-    isSpecialOffer: true
-  },
-
-  {
-    name: 'aloe',
-    category: 'plante grasse',
-    id: '8',
-    isBestSale: true,
-    isSpecialOffer: true
-  },
-
-  {
-    name: 'succulente',
-    category: 'plante grasse',
-    id: '9',
-    isBestSale: false,
-    isSpecialOffer: false
-  },
+    id: '10eb',
+    light: 3,
+    water: 2,
+    cover: avocat
+  }
 ]
